4dffd6f1855fef57b98a6c6734922f10539261b8
devil12820 <152226726+devil12820@users.noreply.github.com>
2024-04-24 21:15:25 +0530

    Update config.py
    Signed-off-by: devil12820 <152226726+devil12820@users.noreply.github.com>

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/9c20ec6fbb5e2eaaafb53.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/5c3456f6a7e807cbcd06f.jpg")
 
 SESSION = getenv("SESSION", None)
 
